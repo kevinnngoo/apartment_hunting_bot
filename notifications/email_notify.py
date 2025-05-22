63e159e3f752805c6e9be60e0ec4150d39cca989
Kevin Ngo <kevinngo2002@gmail.com>
2025-05-21 23:19:28 -0400

    Add email attachment support to email notify.py

--- notifications/email_notify.py
+++ notifications/email_notify.py
@@ -2,12 +2,22 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 
-def send_email(subject, body, to, from_addr, smtp_server, smtp_port, smtp_user, smtp_pass):
+def send_email(subject, body, to, from_addr, smtp_server, smtp_port, smtp_user, smtp_pass, attachment_path=None):
     msg = EmailMessage()
     msg['Subject'] = subject
     msg['From'] = from_addr
     msg['To'] = to
     msg.set_content(body)
+
+    # Attach CSV file if it exists
+    if attachment_path:
+        with open(attachment_path, "rb") as f:
+            msg.add_attachment(
+                f.read(),
+                maintype='application',
+                subtype='octet-stream',
+                filename=attachment_path.split("/")[-1]
+            )
 
     context = ssl.create_default_context()
     try:
